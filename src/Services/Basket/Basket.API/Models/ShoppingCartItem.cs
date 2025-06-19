e342d9b97dc2c4bc0a3499b369e29a29e9242704
Shubham Chauhan (Rx) <radixdt.3305@gmail.com>
2025-06-19 10:54:20 +0530

    Completed  basket service

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCartItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public Guid ProductId { get; set; }
+        public string Color { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
