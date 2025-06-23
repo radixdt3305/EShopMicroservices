1ce380ace457588b7996b65866262d53973ab46f
Shubham Chauhan (Rx) <radixdt.3305@gmail.com>
2025-06-23 17:38:38 +0530

    Completed Communication between basket and discount services

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndPoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndPoint.cs
@@ -1,13 +1,13 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public record GetBasketResponse(ShoppingCart ShoppingCart);
+    public record GetBasketResponse(ShoppingCart Cart);
     public class GetBasketEndPoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("basket/{username}", async (string username, ISender sender) =>
+            app.MapGet("basket/{userName}", async (string userName, ISender sender) =>
             {
-                var query = new GetBasketQuery(username);
+                var query = new GetBasketQuery(userName);
                 var result = await sender.Send(query);
                 var response = result.Adapt<GetBasketResponse>();
                 return Results.Ok(response);
